8b3e3381ed829d1f339fa8933c75e7709fc43aa9
herni <hernanatn@gmail.com>
2025-07-17 13:27:15 -0300

    [DIST] v0.7.8

--- fuente/setup.py
+++ fuente/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='chastack-bdd',
     description="setuptools.build_meta",
-    version='0.7.7',
+    version='0.7.8',
     author           = 'Hernán A. Teszkiewicz Novick',
     author_email     = 'herni@cajadeideas.ar',
     url= 'https://github.com/Hernanatn/bdd.py',
